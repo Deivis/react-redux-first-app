eba75fde63a4d1a3d9e0430454bdc39ccd50143e
deivis <deivis.wingert@labtrans.ufsc.br>
2016-06-07 16:20:42 -0300

    General: created all style files, data files(Which will be use to provide posts and comments to the app) and the initial reduxApp file which will control all the aplication. Comment: That are all the files that came from directly from the tutorial. From now the changes will be made in the reduxApp.js mainly.

---
+++ public/data/posts.js
@@ -0,0 +1,56 @@
+  "id":"1135147611821557699",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12276809_750065668431999_184252508_n.jpg"
+   },
+   {
+      "code":"-1rhFawccs",
+      "caption":"Some major audio upgrades coming to my next videos 😍",
+      "likes":39,
+      "id":"1132002270913873708",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12331333_1650987978502155_1162510634_n.jpg"
+   },
+   {
+      "code":"-pjx-gQcVi",
+      "caption":"My baby and me. Thanks to @bearandsparrow for this one.",
+      "likes":81,
+      "id":"1128590547628442978",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12298962_863814057068027_460827278_n.jpg"
+   },
+   {
+      "code":"-oTZ0zQcWt",
+      "caption":"It's too early. Send coffee.",
+      "likes":81,
+      "id":"1128237044221461933",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xtf1/t51.2885-15/e35/12328347_990748230999662_1512917342_n.jpg"
+   },
+   {
+      "code":"-mxKQoQcQh",
+      "caption":"They both have figured it out. #lifewithsnickers",
+      "likes":47,
+      "id":"1127804966031967265",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xtp1/t51.2885-15/e35/12256736_1758525004381641_1136705181_n.jpg"
+   },
+   {
+      "code":"-fasqlQceO",
+      "caption":"Kaitlin decorated the house for the Christmas. So gezellig! #casabos",
+      "likes":46,
+      "id":"1125735850454402958",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/12277581_1028556737218368_1184190781_n.jpg"
+   },
+   {
+      "code":"-VBgtGQcSf",
+      "caption":"Trying the new Hamilton Brewery beer. Big fan.",
+      "likes":27,
+      "id":"1122810327591928991",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12224456_175248682823294_1558707223_n.jpg"
+   },
+   {
+      "code":"-FpTyHQcau",
+      "caption":"I'm in Austin for a conference and doing some training. Enjoying some local brew with my baby.",
+      "likes":82,
+      "id":"1118481761857291950",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/e35/11326072_550275398458202_1726754023_n.jpg"
+   }
+];
+
+
+export default posts;
