d89c9d268d75aeddb97952709766e104f397f24e
deivis <deivis.wingert@labtrans.ufsc.br>
2016-06-15 19:04:00 -0300

    Tests: added more test cases to the Comments.spec , now the children rendering is validate

--- tests/components/Comments.spec.js
+++ tests/components/Comments.spec.js
@@ -32,16 +32,41 @@ function setup() {
 }
 
 describe('Comments component: ', () => {
-	const { output } = setup();
+	const { output, props } = setup();
+	const [ divs, form ] = output.props.children;
 	
-  it('Should render correctly', () => {
+  it('Should render a div element with the class "comments" ', () => {
 
     expect(output.type).toBe('div');
+    expect(output.props.className).toBe('comments');    
   })
 
-  it('Should have the class "comments"', () => {
-  	
-    expect(output.props.className).toBe('comments');
-  })  
 
+	it('Should render an array of divs and a form as children', () => {  
+    let [div] = divs;
+
+    expect(div.type).toBe('div');
+		expect(div.props.className).toBe('comment');
+
+    expect(form.type).toBe('form');
+    expect(form.props.className).toBe('comment-form');
+  })
+	
+	it('Should the form render tree inputs: author, comment and submit', () => {
+    	let [author,comment,submit] = form.props.children;
+
+			expect(author.type).toBe('input');
+			expect(author.ref).toBe('author');
+			expect(author.props.placeholder).toBe('author');
+
+			expect(comment.type).toBe('input');
+			expect(comment.ref).toBe('comment');
+			expect(comment.props.placeholder).toBe('comment');
+
+			expect(submit.type).toBe('input');
+			expect(submit.props.type).toBe('submit');
+			expect(submit.ref).toNotExist();
+			expect(submit.props.placeholder).toNotExist();
+			expect(submit.props.hidden).toBe(true);
+  })
 })
