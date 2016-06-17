45de4a7b1036675ff2c198889f05fc8337da792d
deivis <deivis.wingert@labtrans.ufsc.br>
2016-06-17 19:25:38 -0300

    Tests: created the PhotoGrid.specs.js which is used to test the PhotoGrid component

---
+++ tests/components/PhotoGrid.spec.js
@@ -0,0 +1,53 @@
+import expect  from 'expect';
+
+import jsdomify from 'jsdomify';
+
+import React from 'react';
+
+import TestUtils from 'react-addons-test-utils';
+
+import PhotoGrid from '../../public/components/PhotoGrid';
+
+import Photo from '../../public/components/Photo';
+
+import comments from '../../public/data/comments';
+
+import posts from '../../public/data/posts';
+
+const setup = () => {
+  const props ={
+   	posts: posts, 
+   	comments: comments, 
+   	increment: expect.createSpy()
+  };
+
+  let renderer = TestUtils.createRenderer();
+  renderer.render(<PhotoGrid {...props}  />);
+  let component = renderer.getRenderOutput();
+
+	return {
+    props,
+    component,
+    renderer
+  }
+}
+
+describe('PhotoGrid component: ', () => {
+	const { props, component, renderer} = setup();
+
+	it('Should render a div with the class "photo-grid" correctly', () => {
+
+		expect(component).toExist();
+		expect(component.type).toBe('div');
+		expect(component.props.className).toBe('photo-grid');
+	})
+
+	it('Should render a list of Photo components', () => {
+		let children = component.props.children;
+		let firstChild = children[0];
+		
+		expect(firstChild).toExist();
+		expect(firstChild.type).toEqual(Photo);
+	})
+
+})
