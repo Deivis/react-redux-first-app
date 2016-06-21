e9a66668dbdc3c0c4f230d567eca00be68bbb5b8
deivis <deivis.wingert@labtrans.ufsc.br>
2016-06-21 11:49:47 -0300

    Tests: Created tests to the routes which will return the posts and comments. The tests will fail because the routes aren't developed.

--- tests/server/routes.spec.js
+++ tests/server/routes.spec.js
@@ -6,7 +6,7 @@ import expect from 'expect';
 
 describe('Routing', () => {
 
-	it('Should do something', (done) =>{
+	it('Should return the main page', (done) =>{
 		request(server)
 		  .get('/')
 		  .expect(200)
@@ -14,8 +14,44 @@ describe('Routing', () => {
 		    if (err) throw err;
 		    
 		    expect(res).toExist('The response object must exist');
-		    	
+
 		    expect(res.ok).toBe(true,'The "ok" must to be true');	
+		    
+		    done();		    
+		  });
+	})
+
+	it('Should return a json with all the posts', (done) =>{
+		request(server)
+		  .get('/posts')
+		  .expect('Content-Type', /json/)
+		  .expect(200)
+		  .end(function(err, res){
+		    if (err) throw err;
+		    
+		    expect(res).toExist('The response object must exist');
+
+		    expect(res.body).toBeA('array','The body must to be an array');	
+
+		    expect(res.body.length).toBeGreaterThan(0,'The response array can not to be empty');	
+		    
+		    done();		    
+		  });
+	})
+
+	it('Should return a json with all the comments', (done) =>{
+		request(server)
+		  .get('/comments')
+		  .expect('Content-Type', /json/)
+		  .expect(200)
+		  .end(function(err, res){
+		    if (err) throw err;
+		    
+		    expect(res).toExist('The response object must exist');
+
+		    expect(res.body).toBeA('array','The body must to be an array');	
+
+		    expect(res.body.length).toBeGreaterThan(0,'The response array can not to be empty');	
 		    
 		    done();		    
 		  });
