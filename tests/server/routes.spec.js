316c0f7dcabfee3cd4dc780c2e88e75829c1277b
deivis <deivis.wingert@labtrans.ufsc.br>
2016-06-21 11:19:54 -0300

    Tests: Added the first integration test which test the default route. General: I will do all the route tests in the same file because this app will have  few routes

--- tests/server/routes.spec.js
+++ tests/server/routes.spec.js
@@ -1,1 +1,24 @@
-//this archive will be used to test the expressroutes which  will return the comments and the posts
+import request from 'supertest';  
+
+import server from '../../server/src/app';
+
+import expect from 'expect';  
+
+describe('Routing', () => {
+
+	it('Should do something', (done) =>{
+		request(server)
+		  .get('/')
+		  .expect(200)
+		  .end(function(err, res){
+		    if (err) throw err;
+		    
+		    expect(res).toExist('The response object must exist');
+		    	
+		    expect(res.ok).toBe(true,'The "ok" must to be true');	
+		    
+		    done();		    
+		  });
+	})
+
+})
